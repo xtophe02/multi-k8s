7a60efb6ef9b73f5f1058920c1c69b4a5a43c51e
xtophe02 <moreira.christophe@outlook.com>
2018-11-01 17:02:57 +0100

    corrected js react

--- client/src/App.js
+++ client/src/App.js
@@ -12,17 +12,14 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">
-              {" "}
-              Fib Calculator version KUBERNETES{" "}
-            </h1>{" "}
-            <Link to="/"> Home </Link> <Link to="/otherpage"> Other Page </Link>{" "}
-          </header>{" "}
+            <h1 className="App-title">Fib Calculator version KUBERNETES</h1>
+            <Link to="/"> Home </Link> <Link to="/otherpage"> Other Page </Link>
+          </header>
           <div>
-            <Route exact path="/" component={Fib} />{" "}
-            <Route path="/otherpage" component={OtherPage} />{" "}
-          </div>{" "}
-        </div>{" "}
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
       </Router>
     );
   }
